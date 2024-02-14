b58b1cac0b021493d9a99250ae6fe86181e62ad7
Kamranbb <kamrannb@code.edu.az>
2024-02-14 14:23:46 +0400

    feat;loopstask

---
+++ looptask/Program.cs
@@ -0,0 +1,96 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region task1
+//int n = 10;
+//int m = 20; 
+//int sum = 0;
+//if (n<m)
+//{
+//    for (int i = n; i < m; i++)
+//    {
+//        if (i%3==0)
+//        {
+//            sum++;
+//        }
+//    } 
+//}
+#endregion
+
+
+
+
+
+//#region task3
+
+//int a= 18;
+//int b= 6;
+//if (a % b == 0)
+//{
+//    Console.WriteLine("Tam bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Bolunmur");
+//}
+
+
+
+
+
+//#endregion
+
+
+
+
+//#region task4 
+//int a1, a2, a3;
+//a1 = 22;
+//a2 = 27;
+//a3 = 28;
+//if (a1 < a2 && a1 < a3)
+
+//{
+//    Console.WriteLine(a2 + a3);
+//}
+
+//else if (a2 < a1 && a2 < a3)
+
+//{ Console.WriteLine(a1 + a3); }
+
+//else if (a3 < a1 && a3 < a2)
+//{
+//    Console.WriteLine(a1 + a2);
+//}
+
+
+
+
+
+
+
+
+
+
+//#endregion
+
+
+
+#region task5   
+//int x = 23;
+//int y = 32;
+//int sum = 0;
+//for (int i = x; i < y; i++)
+//{
+
+//        {
+//        sum += i;
+//    }
+
+
+
+
+//}
+//Console.WriteLine(sum);
+
+#endregion
